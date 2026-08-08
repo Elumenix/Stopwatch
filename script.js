ea929283dac49cbc22691d92ad37b9cc9df95e42
Elumenix <dps5393@g.rit.edu>
2026-08-08 15:13:06 -0400

    fixed broken timer functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Stopwatch")
 
-let seconds = 00;
-let tens = 00;
-let minutes = 00;
+let seconds = 0;
+let tens = 0;
+let minutes = 0;
 let getMinutes = document.querySelector('.minutes')
 let getSeconds = document.querySelector('.seconds')
 let getTens = document.querySelector('.tens')
@@ -10,8 +10,9 @@ let btnStart = document.querySelector('.btn-start')
 let btnStop = document.querySelector('.btn-stop')
 let btnReset = document.querySelector('.btn-reset')
 let interval;
+let running = false;
 
-const startTime = ()=>{
+const startTime = () => {
     tens++;
     if (tens <= 9) {
         getTens.innerHTML = '0' + tens;
@@ -28,7 +29,7 @@ const startTime = ()=>{
     if (seconds > 9) {
         getSeconds.innerHTML = seconds;
     }
-    if(seconds > 59){
+    if (seconds > 59) {
         minutes++;
         getMinutes.innerHTML = '0' + minutes;
         seconds = 0;
@@ -37,14 +38,19 @@ const startTime = ()=>{
 }
 
 btnStart.addEventListener('click', () => {
-   interval = setInterval(startTime, 10);
+    if (!running) {
+        running = true;
+        interval = setInterval(startTime, 10);
+    }
 })
 
 btnStop.addEventListener('click', () => {
+    running = false;
     clearInterval(interval);
 })
 
 btnReset.addEventListener('click', () => {
+    running = false;
     clearInterval(interval);
     tens = '00';
     seconds = '00';
